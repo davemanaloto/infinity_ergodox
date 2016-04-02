3c28d961ec47bed6af44372ce6c5cb6b89ddb0d9
Dave Manaloto <dave.manaloto@gmail.com>
2016-04-01 22:43:12 -0700

    keymap work

--- config.h
+++ config.h
@@ -72,5 +72,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#define NO_ACTION_ONESHOT
 //#define NO_ACTION_MACRO
 //#define NO_ACTION_FUNCTION
+//
+#define TAPPING_TOGGLE 3
 
 #endif
--- keymap_dave.c
+++ keymap_dave.c
@@ -21,20 +21,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KEYMAP(  // layer 0 : default
         // left hand
-        EQL, 1,   2,   3,   4,   5,   ESC,
-        BSLS,Q,   W,   E,   R,   T,   FN1,
-        TAB, A,   S,   D,   F,   G,
-        LSFT,Z,   X,   C,   V,   B,   FN0,
-        LGUI,GRV, BSLS,LEFT,RGHT,
+        GRV,    1,   2,   3,   4,   5,    6,
+        TAB,    Q,   W,   E,   R,   T,   FN1,
+        FN5,   A,   S,   D,   F,   G,
+        LSFT,   Z,   X,   C,   V,   B,   FN0,
+        LGUI,  NO,BSLS,LALT,LGUI,
                                       LCTL,LALT,
                                            HOME,
                                  BSPC,DEL, END,
         // right hand
-             FN2, 6,   7,   8,   9,   0,   MINS,
-             LBRC,Y,   U,   I,   O,   P,   RBRC,
-                  H,   J,   K,   L,   SCLN,QUOT,
+             FN6, 8,   9,   0, MINS, EQL,BSLS,
+             LBRC,Y,   U,   I,    O,   P,RBRC,
+                  H,   J,   K,    L,SCLN,QUOT,
              FN0, N,   M,   COMM,DOT, SLSH,RSFT,
-                       LEFT,DOWN,UP,  RGHT,RGUI,
+                        FN4,RGUI, NO,   NO,  NO,
         RALT,RCTL,
         PGUP,
         PGDN,ENT, SPC
@@ -65,7 +65,7 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left hand
         BTLD,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS, FN3,
-        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+            TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
         TRNS,TRNS,TRNS,TRNS,TRNS,
                                       TRNS,TRNS,
@@ -102,10 +102,58 @@ const uint8_t keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TRNS,
         TRNS,TRNS,TRNS
     ),
+
+    KEYMAP(  // layer 4:
+        // left hand
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,SLCK,PAUS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,TRNS,TRNS,TRNS,
+                                      TRNS,TRNS,
+                                           TRNS,
+                                 TRNS,TRNS,TRNS,
+        // right hand
+             TRNS,TRNS,TRNS,MUTE,VOLD,VOLU,TRNS,
+             TRNS,TRNS,TRNS,MPLY,MRWD,MFFD,TRNS,
+                  LEFT,DOWN,  UP,RGHT,TRNS,TRNS,
+             TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,TRNS,
+                       TRNS,TRNS,TRNS,TRNS,TRNS,
+        TRNS,TRNS,
+        TRNS,
+        TRNS,TRNS,TRNS
+    ),
+
+    /*
+    KEYMAP(  // layer
+        // left hand
+          NO,  NO,  NO,  NO,  NO,  NO,  NO,
+          NO,  NO,  NO,  NO,  NO,  NO,  NO,
+          NO,  NO,  NO,  NO,  NO,  NO,
+          NO,  NO,  NO,  NO,  NO,  NO,  NO,
+          NO,  NO,  NO,  NO,  NO,
+                                        NO,  NO,
+                                             NO,
+                                   NO,  NO,  NO,
+        // right hand
+               NO,  NO,  NO,  NO,  NO,  NO,  NO,
+               NO,  NO,  NO,  NO,  NO,  NO,  NO,
+                    NO,  NO,  NO,  NO,  NO,  NO,
+               NO,  NO,  NO,  NO,  NO,  NO,  NO,
+                         NO,  NO,  NO,  NO,  NO,
+          NO,  NO,
+          NO,
+          NO,  NO,  NO
+    ),
+    */
 };
+
 const uint16_t fn_actions[] = {
     ACTION_LAYER_MOMENTARY(1),                      // FN0 - switch to Layer1
     ACTION_LAYER_SET(2, ON_PRESS),                  // FN1 - set Layer2
     ACTION_LAYER_TOGGLE(3),                         // FN2 - toggle Layer3 aka Numpad layer
     ACTION_LAYER_SET(0, ON_PRESS),                  // FN3 - set Layer0
+    ACTION_LAYER_TAP_TOGGLE(4),                     // FN4 - switch to Layer4 for arrows/fn
+    ACTION_MODS_TAP_KEY(MOD_LCTL, KC_ESC),          // FN5 - left-ctl or ESC when tapped
+    ACTION_LAYER_TAP_KEY(3, KC_7),                  // FN6- 7 or switch to Layer3 when tapped
 };
